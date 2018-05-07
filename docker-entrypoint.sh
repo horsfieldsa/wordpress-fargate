f8ab06f185a82ef56edc00c057f4ab7e5af8c9b8
Scott Hosfield <horsfieldsa@gmail.com>
2018-05-07 13:41:25 -0700

    Fixes typo with enabling IAM role

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -86,7 +86,7 @@ set_config 'DB_HOST' "$WORDPRESS_DB_HOST"
 set_config 'DB_USER' "$WORDPRESS_DB_USER"
 set_config 'DB_PASSWORD' "$WORDPRESS_DB_PASSWORD"
 set_config 'DB_NAME' "$WORDPRESS_DB_NAME"
-set_config 'AWS_USE_EC2_IAM_ROLE' "true"
+set_config 'AS3CF_AWS_USE_EC2_IAM_ROLE' "true"
 set_config 'AS3CF_BUCKET' "$USER_UPLOAD_BUCKET"
 set_config 'AS3CF_REGION' "$USER_UPLOAD_REGION"
 
